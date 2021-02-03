a6fccce9551ee97f9bfe319883b5a9f23011f928
mauricio403 <mauriciomatango_@hotmail.com>
2021-02-03 14:33:54 -0500

    corrigiendo ultimos errores

--- MauricioMatangoFinal/MauricioMatangoFinal/ClientApp/src/app/services/hero.service.ts
+++ MauricioMatangoFinal/MauricioMatangoFinal/ClientApp/src/app/services/hero.service.ts
@@ -1,6 +1,6 @@
-import { Gato } from '../interfaces/gato';
+import { Muricielago } from '../interfaces/murcielago';
 import { Injectable } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+
 import { MessageService } from './message.service';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
@@ -22,46 +22,34 @@ export class HeroService {
               }
 
   //Traer la información desde el servidor
-  getHeroesServidor(): Observable<Gato[]> {
-    return this.http.get<Gato[]>(this.urlHeroes+'/heroes')
+  getHeroesServidor(): Observable<Muricielago[]> {
+    return this.http.get<Muricielago[]>(this.urlHeroes + '/heroes')
       .pipe(
         tap(_ => this.log('fetched heroes')),
-        catchError(this.handleError<Gato[]>('getHeroes', []))
+        catchError(this.handleError<Muricielago[]>('getHeroes', []))
       );
   }
 
-  getHeroServidorId(id: number): Observable<Gato> {
+  getHeroServidorId(id: number): Observable<Muricielago> {
     const url = `${this.urlHeroes}/heroes/${id}`;
-    return this.http.get<Gato>(url).pipe(
+    return this.http.get<Muricielago>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
-      catchError(this.handleError<Gato>(`getHero id=${id}`))
+      catchError(this.handleError<Muricielago>(`getHero id=${id}`))
     );
   }
 
   //Traer la información desde el mock-heroes (YA NO SE USA)
 
-  getHeroes(): Observable<Gato[]> {
+  getHeroes(): Observable<Muricielago[]> {
     this.messageService.add('Servicio de heroes: traer heroes');
     return of(HEROES);
   }
-  getHero(id: number): Observable<Gato> {
+  getHero(id: number): Observable<Muricielago> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
 
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
+ 
 }
